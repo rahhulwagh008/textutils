1679de56038477fc37804d3cf5db5bca941389ce
rahhulwagh008 <rahulwagh446@gmail.com>
2024-06-27 16:36:01 +0530

    Routing Added
    About us page has been routed

--- src/App.js
+++ src/App.js
@@ -7,13 +7,10 @@ import Alert from './component/Alert';
 import {
   BrowserRouter as Router,
   Routes,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 
-
 function App() {
-
   const [mode, setMode] = useState("light");
   const [alert, setAlert] = useState(null);
 
@@ -21,40 +18,38 @@ function App() {
     setAlert({
       message: message,
       type: type
-    })
+    });
 
     setTimeout(() => {
       setAlert(null);
     }, 1000);
-  }
+  };
 
   const toggleMode = () => {
     if (mode === "light") {
       setMode("dark");
       document.body.style.backgroundColor = "lch(14 26.57 274.15)";
       showAlert("Dark mode has been enabled", "success");
-    }
-    else {
+    } else {
       setMode("light");
       document.body.style.backgroundColor = "white";
-      showAlert("Light mode has been enabled", "success")
+      showAlert("Light mode has been enabled", "success");
     }
-  }
+  };
 
   return (
     <>
-
       <Router>
         <Navbar title="Text Utils" about="About Us" mode={mode} toggleMode={toggleMode} />
         <Alert alert={alert}></Alert>
 
         <div className="container">
           <Routes>
-            <Route exact path="/" element={<TextFrom heading="Enter text to analyse" mode={mode}
-              showAlert={showAlert} />} />
+            <Route exact path="/" element={
+              <TextFrom heading="Enter text to analyse" mode={mode} showAlert={showAlert} />
+            }/>
             <Route exact path="/about" element={<About />} />
           </Routes>
-
         </div>
       </Router>
     </>
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 function Navbar(props) {
   return (
-    <nav className={`navbar navbar-expand-lg bg-body-tertiary`}>
+    <nav id="navbar" className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">{props.title}</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -15,7 +15,7 @@ function Navbar(props) {
               <Link className="nav-link active" aria-current="page" to="/about">{props.about}</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/"></Link>
+              <Link className="nav-link" to="/">Home</Link>
             </li>
           </ul>
           <form className="d-flex">
@@ -23,8 +23,8 @@ function Navbar(props) {
             <button className="btn btn-outline-success" type="submit">Search</button>
           </form>
           <div className={`form-check form-switch text-${props.mode === "light" ? "dark" : "light"} mx-3`}>
-            <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" onClick={props.toggleMode} />
             <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable Dark Mode</label>
+            <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" onClick={props.toggleMode} />
           </div>
         </div>
       </div>
